3728d2561bd1b637b02b3cfbbb9c35313e8ef301
zhouqing <zhouqing_mo@163.com>
2022-01-11 15:00:03 +0800

    update

---
+++ src/main/java/org/example/test.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import com.sun.org.apache.xpath.internal.objects.XString;
+
+public class test {
+    public static void main(String[] args)
+    {
+      System.out.println("hello world world");
+    }
+}
